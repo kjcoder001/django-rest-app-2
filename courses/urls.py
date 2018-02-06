f7cc57410225c1d9566d3d827bd9fbb2c3f1a3ee
Kushal Bergkamp <kushalj001@gmail.com>
2018-02-06 11:37:46 +0530

    Added another cbv for retieving,updating,deleting a course and also linked it with a url(takes a primary key)

--- courses/urls.py
+++ courses/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url(r'^$',views.ListCreateCourse.as_view(),name='List Create Course'),
+    url(r'(?P<pk>\d+)/$',views.RetrieveUpdateDestroyCourse.as_view(),name='Course Details'),
+    # the RetrieveUpdateDestroyAPIView expect a key in the url that's named pk
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics # replaces API view and other classes from p
 from . import serializers
 from . import models
 
-
+# This class takes care of listing and creating new courses
 class ListCreateCourse(generics.ListCreateAPIView):
     """Concrete view for listing a queryset or creating a model instance """
 
@@ -12,3 +12,11 @@ class ListCreateCourse(generics.ListCreateAPIView):
     # queryset it will use to find the objects that it needs to listing
     serializer_class=serializers.CourseSerializer
     # specifies the serializer it should use in order to serialize the queryset that it receives
+
+#This class takes care of retrieving,updating and deleting an entry from the database
+class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
+    """ Concrete view for retrieving,updating and deleting a model instance"""
+    queryset=models.Course.objects.all()
+    # uses the query set to go find the data/entry that you asked for,e.g the primary-key you asked for
+    serializer_class=serializers.CourseSerializer
+      
